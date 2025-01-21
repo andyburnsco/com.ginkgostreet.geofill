b6bd23f8ea2d777342a8937ef58c0b7559574d77
Andy Burns <andy@andyburns.co>
2025-01-21 20:19:41 +0000

    run civix upgrade for smarty 5 compatibility

--- geofill.php
+++ geofill.php
@@ -12,17 +12,6 @@ function geofill_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @param array $files
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function geofill_civicrm_xmlMenu(&$files) {
-  _geofill_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -36,18 +25,8 @@ function geofill_civicrm_install() {
 *
 * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
 */
-function geofill_civicrm_postInstall() {
-  _geofill_civix_civicrm_postInstall();
-}
 
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function geofill_civicrm_uninstall() {
-  _geofill_civix_civicrm_uninstall();
-}
+
 
 /**
  * Implements hook_civicrm_enable().
@@ -56,40 +35,6 @@ function geofill_civicrm_uninstall() {
  */
 function geofill_civicrm_enable() {
   _geofill_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function geofill_civicrm_disable() {
-  _geofill_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed
- *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function geofill_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _geofill_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function geofill_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _geofill_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
